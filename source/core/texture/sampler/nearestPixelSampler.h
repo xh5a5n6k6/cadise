1dad0681492ec6d2a36ecfc7f5bd1bea9a82d4a0
xh5a5n6k6 <xh5a5n6k6@gmail.com>
2019-12-29 23:01:07 +0800

    Using PixelSampler template parent constructor

--- source/core/texture/sampler/nearestPixelSampler.h
+++ source/core/texture/sampler/nearestPixelSampler.h
@@ -7,8 +7,19 @@ namespace cadise {
 template<typename T, std::size_t N>
 class NearestPixelSampler : public PixelSampler<T, N> {
 public:
-    using PixelSampler::PixelSampler;
+    using Parent = PixelSampler<T, N>;
+    using Parent::Parent;
     
+    /*
+        When it needs to use template parent's attribute, it need to tell 
+        the compiler explicitly that the names are in fact dependent on 
+        the instantiation of the parent.
+
+        Reference Note:
+        https://stackoverflow.com/questions/6592512/templates-parent-class-member-variables-not-visible-in-inherited-class
+    */
+    using Parent::_uvwWrapper;
+
     void sample(
         const Vector3R& uvw,
         const Image<T, N>& image,
