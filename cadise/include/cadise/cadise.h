3f50f535917c81b0b2b92620086dd012ebf4126e
xh5a5n6k6 <xh5a5n6k6@gmail.com>
2022-05-01 18:50:50 +0800

    Add more cadise apis

--- cadise/include/cadise/cadise.h
+++ cadise/include/cadise/cadise.h
@@ -1,12 +1,24 @@
 #pragma once
 
-// HACK: only include files used in cadise-cli
-// TODO: remove this after better Cadise API design
-#include <file-io/scene-description/sdParser.h>
-
-
 #define CS_API
 
+// primitive type
+#include <stdint.h>
+
+typedef int8_t      CSint8;
+typedef uint8_t     CSuint8;
+typedef int16_t     CSint16;
+typedef uint16_t    CSuint16;
+typedef int32_t     CSint32;
+typedef uint32_t    CSuint32;
+typedef int64_t     CSint64;
+typedef uint64_t    CSuint64;
+
+typedef float       CSfloat32;
+typedef double      CSfloat64;
+
+typedef bool        CSbool;
+typedef char        CSchar;
 
 #define CS_TRUE  1
 #define CS_FALSE 0
@@ -15,17 +27,34 @@
 extern "C" {
 #endif
 
-/*
-    Initilize the Cadise render engine.
+/*! @brief Initilize the Cadise render engine.
 */
-extern CS_API int csInit();
+extern CS_API CSbool csInit();
 
-/*
-    Exit the Cadise render engine.
+/*! @brief Exit the Cadise render engine.
 */
-extern CS_API int csExit();
+extern CS_API CSbool csExit();
 
-//extern CS_API void csRender();
+extern CS_API void csCreateRenderDatabase(CSuint64* const out_renderDatabaseId);
+
+extern CS_API void csDeleteRenderDatabase(const CSuint64 renderDatabaseId);
+
+/*! @brief Parse scene description file (with .crsd extension) and set up to specified render database.
+
+@return Value to indicate parsing procedure successful or not.
+*/
+extern CS_API CSbool csParseFile(const CSuint64 renderDatabaseId, const CSchar* filename);
+
+/*! @brief Specify an engine to start render progress synchronously.
+*/
+extern CS_API void csRender(const CSuint64 renderDatabaseId);
+
+/*! @brief Specify an engine to start render progress asynchronously.
+
+The only difference between csRender is that it will call another thread
+to do the render job, which means this function won't block the process.
+*/
+extern CS_API void csRenderAsync(const CSuint64 renderDatabaseId);
 
 #ifdef __cplusplus
 }
--- cadise/source/api/cadise.cpp
+++ cadise/source/api/cadise.cpp
@@ -1,15 +1,68 @@
 #include "cadise/cadise.h"
 
-int csInit()
+#include "api/globalStorage.h"
+#include "core/renderDatabase.h"
+#include "file-io/scene-description/sdParser.h"
+#include "fundamental/assertion.h"
+
+#include <vector>
+
+using namespace cadise;
+
+CSbool csInit()
 {
     return CS_TRUE;
 }
 
-int csExit()
+CSbool csExit()
 {
     return CS_TRUE;
 }
 
-//void csRender() {
-//
-//}
+void csCreateRenderDatabase(CSuint64* const out_renderDatabaseId)
+{
+    CS_ASSERT(out_renderDatabaseId);
+
+    *out_renderDatabaseId = GlobalStorage::createResource<RenderDatabase>();
+}
+
+void csDeleteRenderDatabase(const CSuint64 renderDatabaseId)
+{
+    GlobalStorage::deleteResource<RenderDatabase>(renderDatabaseId);
+}
+
+CSbool csParseFile(const CSuint64 renderDatabaseId, const CSchar* filename)
+{
+    auto renderDatabase = GlobalStorage::getResource<RenderDatabase>(renderDatabaseId);
+    if (renderDatabase)
+    {
+        SdParser sdParser;
+
+        std::vector<std::shared_ptr<SdData>> sdDatas;
+        sdParser.parse(std::string(filename), renderDatabase.get(), sdDatas);
+        //std::cout << sdDatas.size();
+        //for (auto& data : sdDatas)
+        //{
+        //    renderDatabase->setUpData(data);
+        //}
+
+        return CS_TRUE;
+    }
+
+    return CS_FALSE;
+}
+
+void csRender(const CSuint64 renderDatabaseId)
+{
+    auto renderDatabase = GlobalStorage::getResource<RenderDatabase>(renderDatabaseId);
+    if (renderDatabase)
+    {
+        renderDatabase->prepareRender();
+        renderDatabase->render();
+    }
+}
+
+void csRenderAsync(const CSuint64 renderDatabaseId)
+{
+    // TODO
+}
