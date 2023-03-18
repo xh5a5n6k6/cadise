f145d5937660633f50657149e81a8c8e74243caa
xh5a5n6k6 <xh5a5n6k6@gmail.com>
2023-03-18 22:28:40 +0800

    Add disk sampling

---
+++ Cadise/Source/Math/Warp/Disk.cpp
@@ -0,0 +1,78 @@
+#include "Math/Warp/Disk.h"
+
+#include "Foundation/Assertion.h"
+#include "Math/Constant.h"
+#include "Math/TVector2.h"
+
+#include <cmath>
+
+namespace cadise::internal
+{
+
+// Return (radius, theta)
+std::pair<real, real> mapToRadiusTheta(Vector2R sample)
+{
+    if (std::abs(sample[0]) > std::abs(sample[1]))
+    {
+        return
+        {
+            sample[0],
+            constant::rcp_four_pi<real> * (sample[1] / sample[0])
+        };
+    }
+    else
+    {
+        return
+        {
+            sample[1],
+            constant::rcp_two_pi<real> - constant::rcp_four_pi<real> *(sample[0] / sample[1])
+        };
+    }
+}
+
+} // namespace cadise::internal
+
+namespace cadise 
+{
+
+void Disk::uniformSampling(
+    const std::array<real, 2>& sample,
+    Vector2R* const            out_surfacePoint) 
+{
+    CS_ASSERT(out_surfacePoint);
+
+    const real sampledRadius = std::sqrt(sample[0]);
+    const real sampledTheta  = constant::two_pi<real> * sample[1];
+    const real cosTheta      = std::cos(sampledTheta);
+    const real sinTheta      = std::sqrt(1.0_r - cosTheta * cosTheta);
+
+    out_surfacePoint->set(
+        sampledRadius * cosTheta,
+        sampledRadius * sinTheta);
+}
+
+void Disk::concentricSampling(
+    const std::array<real, 2>& sample,
+    Vector2R* const            out_surfacePoint) 
+{
+    CS_ASSERT(out_surfacePoint);
+
+    // Map sample from [0, 1] to [-1, 1]
+    const Vector2R mappedSample = Vector2R(sample).mul(2.0_r).sub(1.0_r);
+    if (mappedSample.isZero())
+    {
+        out_surfacePoint->set(0.0_r);
+        return;
+    }
+
+    const auto [sampledRadius, sampledTheta] = internal::mapToRadiusTheta(mappedSample);
+
+    const real cosTheta = std::cos(sampledTheta);
+    const real sinTheta = std::sqrt(1.0_r - cosTheta * cosTheta);
+
+    out_surfacePoint->set(
+        sampledRadius * cosTheta,
+        sampledRadius * sinTheta);
+}
+
+} // namespace cadise
---
+++ Cadise/Source/Math/Warp/Disk.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Math/Type/MathType.h"
+
+#include <array>
+
+namespace cadise 
+{
+
+// Reference: PBRT-v3
+class Disk
+{
+public:
+    static void uniformSampling(
+        const std::array<real, 2>& sample, 
+        Vector2R* const            out_surfacePoint);
+
+    static void concentricSampling(
+        const std::array<real, 2>& sample,
+        Vector2R* const            out_surfacePoint);
+};
+
+} // namespace cadise
